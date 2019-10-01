675babc8d716bbf85f62eaf337df281b639e3c3f
DiegoAndino <andino.diego98@gmail.com>
2019-10-01 17:36:10 -0600

    Game Project Init - 1st Main Menu Iteration

---
+++ Assets/Scripts/Scenes/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+	public void GoToGameScene()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void GoToMenuScene()
+	{
+		SceneManager.LoadScene("Main Menu");
+	}
+
+    public void QuitGame()
+	{
+		Application.Quit();
+		print("GAME IS QUITTING . . .");
+	}
+}
