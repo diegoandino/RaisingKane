bace8c6a65d2b617e880a809432c3d7f11b76105
Rachel <tailsnick@yahoo.com>
2019-10-14 03:21:25 -0600

    Rhythm mechanic implemented into main
    The rhythm mechanic is still based of the video that I posted, but it has 2 unique features in the Game Manger.
    
    1- There is a delay timer for the offset to allow you to give the player just enough time before the beat starts.
    
    2- songSpeed which changes the pitch/tempo based of the "percentage" you give it, allowing you to position beats better to the rhythm when it goes in regular speed.

---
+++ Assets/Scripts/Rhythm/GameManger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public AudioSource Music;
+
+    public bool startPlaying = false;
+
+    public RhythmSpeed RSpeed;
+
+    public static GameManger instance;
+
+    public float StartOffset;
+
+    public float songSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        Music.pitch = songSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                RSpeed.hasStarted = true;
+
+                Music.PlayDelayed(StartOffset/songSpeed);
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit");
+    }
+
+    public void NoteMiss()
+    {
+        Debug.Log("Miss");
+    }
+}
