e9288c553ea01291cce69cd2c221ca2757aaf3a1
DiegoAndino <andino.diego98@gmail.com>
2019-10-14 17:02:03 -0600

    Added to the main Game scene
    Really basic script and layout

---
+++ Assets/Scripts/Player/PLAYER_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER_Movement : MonoBehaviour
+{
+    public float MOVEMENT_SPEED = 0f;
+    public Animator Animator;
+
+    private bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        {
+            float move = Input.GetAxis("Horizontal");
+            Animator.SetFloat("Speed", move); 
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            isRight = true;
+
+            transform.Translate(Vector3.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            isRight = false; 
+
+            transform.Translate(Vector3.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+}
