39bf1773e3014a3f2aa113d632bf7e9507c691a4
r2shef2 <43525252+r2shef2@users.noreply.github.com>
2019-10-27 15:04:00 -0600

    Rhythm art update

--- Assets/Scripts/Rhythm/ShrinkingRythm.cs
+++ Assets/Scripts/Rhythm/ShrinkingRythm.cs
@@ -7,6 +7,7 @@ public class ShrinkingRythm : MonoBehaviour
     public float Timer;
     public float scale = 15;
     public float shrinkMod = 1;
+    public float rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,9 @@ public class ShrinkingRythm : MonoBehaviour
     {
         scale = scale - (Time.deltaTime * shrinkMod);
         transform.localScale = new Vector3(scale, scale, scale);
+        // rotates on the z based on a serializable variable and time
+        //transform.localRotation = new Quaternion(0, 0, Time.deltaTime * rotation, 0);'
+        transform.Rotate(0, 0, rotation);
         if (scale < 0.08)
         {
             gameObject.GetComponentInParent<ShrinkRythmManager>().DisCheck(-10f);
