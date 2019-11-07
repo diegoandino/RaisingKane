90dcbc15c47353a7e31e69b8fffd6ae51dc4250f
r2shef2 <43525252+r2shef2@users.noreply.github.com>
2019-11-07 15:09:10 -0700

    adding temp art

---
+++ Assets/DoorCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorCollision : MonoBehaviour
+{
+
+    public string roomName;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Do something else here");
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Do something else here");
+            SceneManager.LoadScene(roomName, LoadSceneMode.Additive);
+        }
+    }
+}
--- Assets/Scripts/Player/Collision.cs
+++ Assets/Scripts/Player/Collision.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Collision : MonoBehaviour
     public GameObject rhythmMechanic;
     private bool collided; //bool for if we have collided with a door
     private ShrinkRythmManager shrinkManager;
+    public string nextRoom;
 
     // Start is called before the first frame update
     void Start()
