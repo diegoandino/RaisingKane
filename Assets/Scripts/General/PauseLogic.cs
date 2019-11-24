4ed6e87c6b168b4fb2b4d7bc1d531e724e11295a
Kelsey <kelseyfink@me.com>
2019-11-24 13:32:05 -0700

    Pause Menu Progress

---
+++ Assets/Scripts/General/PauseLogic.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseLogic : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+        //pauseMenu = GetComponent<GameObject>();
+        //canvas = canvas.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        isPaused = false;
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        isPaused = true;
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void DisplayControls()
+    {
+        Debug.Log("displaying controls");
+    }
+
+    public void ReturnToHub()
+    {
+        SceneManager.LoadScene("Gymnasium Hub");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
