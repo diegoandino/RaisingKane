29d79932abfc392edb328450dc27f44df94f4e0c
Kelsey <kelseyfink@me.com>
2019-11-25 12:32:29 -0700

    Added in Monster Animations

---
+++ Assets/Scripts/Rhythm/MonsterAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator MonsterAnimateController;
+    public BeatGenerator bg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MonsterAnimateController.SetBool("playAttack", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bg.start)
+        {
+            MonsterAnimateController.SetBool("playAttack", true);
+        }
+    }
+}
