4138e901e3566e7f10c55afe842ea9f67e7e0a5a
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2020-01-16 00:31:24 -0700

    Completed 1/2 of alt controller. Added camera display feed and capture, and how to replace another image with the screenshot

---
+++ Assets/Scripts/Alt Controller/LoadImage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadImage : MonoBehaviour
+{
+    public SpriteRenderer test;
+    public Sprite original;
+    public Sprite replacement;
+
+    public void Start()
+    {
+        test.sprite = original;
+    }
+
+    public void replace()
+    {
+        test.sprite = replacement;
+    }
+
+}
--- Assets/Scripts/Alt Controller/WebCam.cs
+++ Assets/Scripts/Alt Controller/WebCam.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class WebCam : MonoBehaviour
-{
+{   
+    //Webcam Declarations
     int currentCamIndex = 0;
 
     WebCamTexture tex;
-    private string _SavePath = "C:/Users/u1082216/Documents/GitHub/RaisingKane/Assets/Art/Screenshots/";
+    private string _SavePath = @"Assets\Resources\";
     public RawImage display;
+    public Sprite replacementSprite;
     int _CaptureCounter = 0;
+
+    //Sprite Renderers
+    public SpriteRenderer testImage;
     public SpriteRenderer CameraDetectedCheckMark;
     public SpriteRenderer pictureTakenCheckMark;
     public SpriteRenderer replaceImageCheckMark;
-    public SpriteRenderer testImage;
-    public Sprite original;
-    public Sprite replacement;
+    
 
 
 
@@ -35,6 +38,7 @@ public class WebCam : MonoBehaviour
     //ACTIVATING CAMERA
     public void startStopCamera_Clicked()
     {
+        //if no camera deteced, declare camera null
         if (tex != null)
         {
             display.texture = null;
@@ -43,6 +47,7 @@ public class WebCam : MonoBehaviour
 
 
         }
+        //if camera is deteced, cast video as a texture to the raw image
         else
         {
             WebCamDevice device = WebCamTexture.devices[currentCamIndex];
@@ -50,59 +55,65 @@ public class WebCam : MonoBehaviour
             display.texture = tex;
             display.material.mainTexture = tex;
             tex.Play();
+
+            //play checkmark camera detected
             CameraDetectedCheckMark.transform.position = new Vector3(CameraDetectedCheckMark.transform.position.x, CameraDetectedCheckMark.transform.position.y, -2);
         }
 
     }
 
-    //TAKING A PICTURE
+    //TAKING A PICTURE AND STORING THE FILE AS A SPRITE
     public void takePicture_Clicked()
     {
-
+        //play checkmark picture taken
         pictureTakenCheckMark.transform.position = new Vector3(pictureTakenCheckMark.transform.position.x, pictureTakenCheckMark.transform.position.y, -2);
 
+        //capture the current texture feed
         Texture2D screenshot = new Texture2D(tex.width, tex.height);
+        //turn the feed into a pack of pixels
         screenshot.SetPixels(tex.GetPixels());
         screenshot.Apply();
 
+        //encode the pixel info as a png type
         byte[] bytes = screenshot.EncodeToPNG();
 
+        //writing the png into a file at the directory specified
         System.IO.File.WriteAllBytes(_SavePath + _CaptureCounter.ToString() + ".png", screenshot.EncodeToPNG());
 
+        //turning the screenshot texture into a Sprite
+
+        //creating the containment parameters of the sprite
+        Rect rect = new Rect(0, 0, screenshot.width, screenshot.height);
+
+        //seeking vector pivot points of sprite
+        Vector2 vect = new Vector2(.5f, .5f);
+
+        //creating the replacement sprite from the screenshot
+        replacementSprite = Sprite.Create(screenshot, rect, vect, 80);
+        
+        
+        //if more than one screenshot is allowed, name it ++ (0, 1, 2, etc)
         ++_CaptureCounter;
 
+        //stop camera feed (DISABLE TO KEEP CAMERA LIVE)
         tex.Stop();
     }
 
-    //REPLACING IMAGE FROM DIRECTORY
+    //REPLACING IMAGE FROM CURRENT QUEUED SCREENSHOT
     public void replaceImage_Clicked()
     {
+        //play checkmark replaced image
         replaceImageCheckMark.transform.position = new Vector3(replaceImageCheckMark.transform.position.x, replaceImageCheckMark.transform.position.y, -2);
 
-        getImage();
+              //GameObject replacement = GameObject.Find("Test");
 
-    }
+              //replacement.GetComponent<SpriteRenderer>().sprite = replacementSprite;
 
-    //GETTING IMAGE FROM DIRECTORY
-    public void getImage()
-    {
+        //replacing the original image with the replacement Sprite
+        testImage.sprite = replacementSprite;
         
     }
 
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        testImage.sprite = original;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
 }
 
    
