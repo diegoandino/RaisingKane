cce4230e2ef382a21586242ab6ce21ce9774d2b1
David Ross <davidr85226@yahoo.com>
2020-01-25 12:13:31 -0700

    Beat Rotation Working

--- Assets/Scripts/Rhythm/PROJECTILES/ArchProjectile.cs
+++ Assets/Scripts/Rhythm/PROJECTILES/ArchProjectile.cs
@@ -8,8 +8,9 @@ using UnityEngine;
 public class ArchProjectile : Projectile
 {
     public  float BPM = 86;           //Tells how fast the rhythm is going. (BPM - Beats per minute)
-    public  float laneSpeed;     //added Lanespeed for the different arcs of the lanes
+    public  float dropSpeed;     //added Lanespeed for the different arcs of the lanes
     public  float ProjectileSpeed;
+    public float ArchMod; //Higher the Value, higher the arch;
     private float count = 0.0f;
     private List<Vector3> points;
 
@@ -24,9 +25,9 @@ public class ArchProjectile : Projectile
         StartPoint = this.transform.position;
         points = new List<Vector3>();
 
-        points.Add(StartPoint);
-        points.Add(EndPoint);
-        points.Add(points[0] + (points[1] - points[0]) / 2 + Vector3.up * 60.0f);
+        points.Add(new Vector3 (StartPoint.x,StartPoint.y, 1));
+        points.Add(new Vector3(EndPoint.x, EndPoint.y, 1));
+        points.Add(points[0] + (points[1] - points[0]) / 2 + Vector3.up * ArchMod);
     }
 
     // Update is called once per frame
@@ -43,12 +44,25 @@ public class ArchProjectile : Projectile
                 Vector3 m1 = Vector3.Lerp(points[0], points[2], count);
                 Vector3 m2 = Vector3.Lerp(points[2], points[1], count);
                 transform.position = Vector3.Lerp(m1, m2, count);
+
+                // Determine which direction to rotate towards
+                Vector3 targetDirection = m2 - transform.position;
+                Quaternion rot = Quaternion.LookRotation(targetDirection, Vector3.up);
+                Quaternion NewRot = Quaternion.identity;
+                NewRot.eulerAngles = new Vector3 (0, 0, (rot.eulerAngles.x-90));
+                transform.rotation = NewRot;
+
             }
 
             if (count > 1f)
             {
-                transform.position -= new Vector3(0f, (BPM * Time.deltaTime) * BP2_MusicSettings.instance.songSpeed * laneSpeed, 0f);
+                transform.position -= new Vector3(0f, (BPM * Time.deltaTime) * BP2_MusicSettings.instance.songSpeed * dropSpeed, 0f);
+                Quaternion downRot = Quaternion.identity;
+                downRot.eulerAngles = new Vector3(0, 0, 0);
+                transform.rotation = downRot;
             }
         }
     }
+
+
 }
