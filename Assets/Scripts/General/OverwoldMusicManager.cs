960e19ba630626e09b705dc15ab76235ff69c92d
Kelsey <kelseyfink@me.com>
2020-03-22 19:21:05 -0600

    Updated options menu volume slider

--- Assets/Scripts/General/OverwoldMusicManager.cs
+++ Assets/Scripts/General/OverwoldMusicManager.cs
@@ -6,17 +6,19 @@ public class OverwoldMusicManager : MonoBehaviour
 {
 
     public float defaultMusicValue;
+    private AudioSource audioSrc;
+    private float musicVolume = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        audioSrc.volume = musicVolume;
     }
 
     //
@@ -32,7 +34,10 @@ public class OverwoldMusicManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+    }
 
-
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
     }
 }
